639d6d45836ef978d3b0198e549541b60929a56e
IKIM <Ecris ton email ici, en gardant les quotes>
2022-05-20 17:09:52 +0200

    upload pages post, post detail

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\CommentRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
